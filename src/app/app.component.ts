92b0cf220567e81b17428ba437221f392f85e84f
Niklas @yDev <niklas@magic-computer.de>
2020-10-10 18:02:54 +0200

    Picture update, bug fix (no slide view without login)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {UserdataService} from './services/userdata.service';
 
 @Component({
   selector: 'app-root',
@@ -47,7 +48,8 @@ export class AppComponent implements OnInit {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    public userdata: UserdataService
   ) {
     this.initializeApp();
   }
