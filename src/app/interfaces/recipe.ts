00bbde49565fda3f5c9bcac13b858c4546a8ee81
Christian Schliz <code@foxat.de>
2020-10-19 11:16:08 +0200

    hrngngnrngngngnngnn

--- src/app/interfaces/recipe.ts
+++ src/app/interfaces/recipe.ts
@@ -1,10 +1,12 @@
+import { Preference } from './preference';
+
 export interface Recipe {
     id: string;
     title: string;
     subtitle: string;
     imageURL: string;
     blurhash: string;
-    preferences: string;
+    preferences: Preference[];
     steps: {
         index: number;
         title: string;
