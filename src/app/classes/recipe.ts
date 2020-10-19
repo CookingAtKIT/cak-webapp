13ae77eb5ca11e25d6835278b9f54d82571ae6b4
yJulian <yJulian@outlook.de>
2020-10-19 12:03:25 +0200

    Add Recipe class Add Connection Service

---
+++ src/app/classes/recipe.ts
@@ -0,0 +1,45 @@
+export class Recipe {
+    id: string;
+    public: boolean;
+    flags: number;
+    title: string;
+    author: string;
+    lastEdit: Date;
+    thumbnail: string;
+    ingredients: {
+        ingredient: {
+            displayname:string,
+            allgergene: string[],
+            unit: string[],
+            kcal?: number,
+            reweLink?: string
+        },
+        amount: number
+    }[];
+    steps: {
+        index: number,
+        title: string,
+        body: string,
+        img?: string
+    }[];
+    likes: number;
+    portions: number;
+    comments: {
+        author: string,
+        body: string,
+        likes: number,
+        images: string[]
+    };
+}
+
+export class RecipeEdit {
+    edits?: {any}[];
+    error?: string;
+    description?: string;
+}
+
+export class RecipeCreate {
+    id?: string;
+    error?: string;
+    description?: string;
+}
